8e0cf31789e1aeed2a448c02224bdd7aaf6ba1ca
Marmot-ojisan <142467999+Marmot-ojisan@users.noreply.github.com>
2023-09-20 12:33:30 +0900

    課題5の提出

---
+++ kadai_005/bmi.js
@@ -0,0 +1,15 @@
+// 定数の宣言・値の代入
+let taiju; // 体重
+let sincho; // 身長
+
+taiju = 68; // 体重
+sincho = 1.7; // 身長
+
+// 変数の宣言
+let bmi;
+
+// BMIの計算
+bmi = taiju / (sincho * sincho);
+
+// コンソールへの出力
+console.log(bmi);
